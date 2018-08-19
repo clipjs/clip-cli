a3c3d469b021906c30dd6fb22c61185c4d831669
Matheus <matheus@despossivel.pinpper.com>
2018-08-19 20:46:33 -0300

    Testando comacomando RUN

---
+++ lib/core_/core_sh/pinpper/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+chmod 777 -R "./" 
+
+gnome-terminal --execute bash -c "./1.sh > /home/matheus/Documentos/pinpper/Clip/lib/core_/struct_/../core_sh/pinpper/logs/clip.log; bash"
+
+gnome-terminal --execute bash -c "./2.sh > /home/matheus/Documentos/pinpper/Clip/lib/core_/struct_/../core_sh/pinpper/logs/clip.log; bash"
+
+gnome-terminal --execute bash -c "./3.sh > /home/matheus/Documentos/pinpper/Clip/lib/core_/struct_/../core_sh/pinpper/logs/clip.log; bash"
+
+gnome-terminal --execute bash -c "./4.sh > /home/matheus/Documentos/pinpper/Clip/lib/core_/struct_/../core_sh/pinpper/logs/clip.log; bash"
